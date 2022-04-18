b98ebc8060724f30c24134d14b2196e09f085b70
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-04-18 16:48:39 -0400

    got the backend running for get all categories

---
+++ Tabloid/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        private readonly ICategoryRepository _categoryRepo;
+
+        public CategoryController(ICategoryRepository catRepo)
+        {
+            _categoryRepo = catRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Getall()
+        {
+            return Ok(_categoryRepo.GetAll());
+        }
+    }
+}
---
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Category> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name From Category";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var categories = new List<Category>();
+
+                        while (reader.Read())
+                        {
+                            categories.Add(new Category()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            });
+                        }
+
+                        return categories;
+                    }
+                }
+            }
+        }
+
+    }
+}
