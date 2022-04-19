643eacb5d6b0446fa9f2b5c1b102407836d50e2e
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-04-19 14:16:50 -0400

    added authorization checking

--- Tabloid/client/src/modules/CategoryManager.js
+++ Tabloid/client/src/modules/CategoryManager.js
@@ -1,5 +1,22 @@
+import { getToken } from "./authManager";
+
 const baseUrl = `/api/category`;
 
 export const getAllCategories = () => {
-  return fetch(baseUrl).then((res) => res.json());
+  return getToken().then((token) => {
+    return fetch(baseUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error(
+          "An unknown error occurred while trying to get quotes."
+        );
+      }
+    });
+  });
 };
