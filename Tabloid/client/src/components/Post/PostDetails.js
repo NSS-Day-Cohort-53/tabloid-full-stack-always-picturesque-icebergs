616165645f237d08613e7c766d7d5dc82dc66faf
SchmidtFred <fschmidt@live.com>
2022-04-20 10:54:01 -0500

    (+) post title is not a link when on details page

--- Tabloid/client/src/components/Post/PostDetails.js
+++ Tabloid/client/src/components/Post/PostDetails.js
@@ -20,7 +20,7 @@ const PostDetails = () => {
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-sm-12 col-lg-6">
-                    <Post post={post} />
+                    <Post post={post} hasTitleLink={false} />
                     <ListGroup>
                         {/* TODO - Comments will be listed here on a future ticket */}
                     </ListGroup>
