47a7a1ca6b2b11de25efa34e61ab826f254b9298
SchmidtFred <fschmidt@live.com>
2022-04-21 15:01:42 -0500

    (+)form complete

--- Tabloid/client/src/components/Post/CreatePost.js
+++ Tabloid/client/src/components/Post/CreatePost.js
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { addPost } from "../../modules/postManager";
 import { getAllCategories } from "../../modules/CategoryManager";
+import { useHistory } from "react-router-dom";
 
 const CreatePost = () => {
     const [post, setPost] = useState({
         title: "",
         content: "",
         imageLocation: "",
-        publishDateTime: "",
-        categoryId: ""
+        publishDateTime: null,
+        categoryId: 0
     });
     const [categories, setCategories] = useState([]);
+    const history = useHistory();
 
     useEffect(() => {
         getAllCategories().then(setCategories);
@@ -22,31 +24,61 @@ const CreatePost = () => {
         let value = target.value;
         const name = target.name;
         const copy = {...post};
-        //conver categoryId to an int
+        //convert categoryId to an int
         if (name === "categoryId") {
             value = parseInt(value);
         }
+        //convert publishDateTime to a datetime
+        if (name === "publishDateTime") {
+            value = Date.parse(value);
+        }
         copy[name] = value;
         setPost(copy);
-    }
+    };
+
+    const savePost = (event) => {
+        event.preventDefault();
+        const copy = {...post};
+        //handle nullable/required properties
+        if (copy.categoryId === 0) {
+            window.alert("Please select a category.")
+        }
+        if (copy.imageLocation === "") {
+            copy.imageLocation = null;
+        }
+        addPost(copy)
+            .then((newPost) => history.push(`/posts/${newPost.id}`))
+            // .catch(() => alert("Invalid Post Submission"));
+    };
 
     return (
-        <Form>
+        <Form onSubmit={savePost}>
             <fieldset>
                 <FormGroup>
                     <Label for="title">Title</Label>
-                    <Input id="title" name="title" type="text" onChange={handleUserInput} />
+                    <Input id="title" name="title" type="text" onChange={handleUserInput} required/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="categoryId">Category</Label>
-                    <Input type="select" id="categoryId" name="categoryId" onChange={handleUserInput}>
-                        <option value="">Select a Category</option>
+                    <Input type="select" id="categoryId" name="categoryId" onChange={handleUserInput} required>
+                        <option value="0">Select a Category</option>
                         {categories.map(c => <option key={c.id} value={c.id}>{c.name}</option>)}
                     </Input>
                 </FormGroup>
                 <FormGroup>
                     <Label for="publishDateTime">Publish Date</Label> 
-                    <Input type="datetime" id="publishDateTime" name="publishDateTime" onChange={handleUserInput} />
+                    <Input type="date" id="publishDateTime" name="publishDateTime" onChange={handleUserInput} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="imageLocation">Header Image Link</Label>
+                    <Input type="url" id="imageLocation" name="imageLocation" onChange={handleUserInput} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="content">Body</Label>
+                    <Input type="textarea" id="content" name="content" onChange={handleUserInput} required/>
+                </FormGroup>
+                <FormGroup>
+                    <Button>Save</Button>
                 </FormGroup>
             </fieldset>
         </Form>
